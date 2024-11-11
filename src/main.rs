c1c1e8d4c0eba34fbb2dd69c83eadfd68bc2b85d
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-11-11 16:51:53 -0300

    Enhance main.rs to better display steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use prettytable::{cell, row, Table};
+use std::io;
+
 pub mod block;
 pub mod blockchain;
 pub mod ledger;
@@ -5,11 +8,23 @@ pub mod loadcsv;
 pub mod transaction;
 
 fn main() {
+    let mut input = String::new();
+    println!("Press Enter to create new blockchain with genesis block.");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
     let mut bc = blockchain::Blockchain::new();
+    println!("Blockchain created: {:?}", bc.ledger);
+
+    println!("Enter to load transactions from CSV file...");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
     let transactions = loadcsv::load_transactions_from_csv("transactions.csv")
         .expect("Failed to load transactions");
-    println!("{:?}", transactions);
+    println!("Loaded the set of transactions:");
 
     // let transactions = vec![
     //     transaction::Transaction::new("Alice".to_string(), "Bob".to_string(), 10.0),
@@ -19,6 +34,18 @@ fn main() {
     //     transaction::Transaction::new("Bob".to_string(), "Bob".to_string(), 5.0),
     //     transaction::Transaction::new("Charlie".to_string(), "Alice".to_string(), 15.0),
     // ];
+
+    let mut table = Table::new();
+    table.add_row(row!["Origin", "Destination", "Value"]);
+    for tx in &transactions {
+        table.add_row(row![tx.origin, tx.destination, tx.value]);
+    }
+    table.printstd();
+
+    println!("Press Enter to add transactions to the blockchain");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
     for tx in transactions {
         bc.add_transaction(tx).expect("Failed to add transaction");
